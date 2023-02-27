54feaf197b9da00078a5e1864353d75c8e8ee308
Anish Mainali <102987720+robin-f-00@users.noreply.github.com>
2023-02-28 00:11:09 +0530

    Update doctor.java

--- doctor.java
+++ doctor.java
@@ -1,1 +1,12 @@
 import java.util.*;
+
+public class project {
+        public class Doctor {
+            private String name;
+            private String speciality;
+            private String phone;
+            // other attributes as needed
+
+            // getters and setters for each attribute
+        }
+    }
