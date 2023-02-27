68d9e5226a7c3c6ad1263d9e3af09d560799f5e8
Anish Mainali <102987720+robin-f-00@users.noreply.github.com>
2023-02-28 00:16:45 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SurgerySystem {
+    private ArrayList<Doctor> doctors;
+    private ArrayList<Patient> patients;
+
+    public SurgerySystem() {
+        doctors = new ArrayList<>();
+        patients = new ArrayList<>();
+    }
