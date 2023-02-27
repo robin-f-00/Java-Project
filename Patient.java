bc9a3a2bdda08e48fd662fea51a0d92e722e380c
Anish Mainali <102987720+robin-f-00@users.noreply.github.com>
2023-02-28 00:14:40 +0530

    Create Patient.java

---
+++ Patient.java
@@ -0,0 +1,16 @@
+public class Patient {
+    private String name;
+    private int age;
+    private String address;
+    private String phone;
+    // other attributes as needed
+
+    public Patient(String name, int age, String address, String phone) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    // getters and setters for each attribute
+}
