5b5d20fcbabc515e91d47947cb4ff219173be4d4
Anish Mainali <102987720+robin-f-00@users.noreply.github.com>
2023-02-28 00:12:32 +0530

    Update doctor.java

--- doctor.java
+++ doctor.java
@@ -6,7 +6,11 @@ public class project {
             private String speciality;
             private String phone;
             // other attributes as needed
-
+  public Doctor(String name, String speciality, String phone) {
+                this.name = name;
+                this.speciality = speciality;
+                this.phone = phone;
+            }
             // getters and setters for each attribute
         }
     }
